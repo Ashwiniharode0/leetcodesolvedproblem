de15eea2f1972a1fc2a964739e9d6bf3f4af0705
Ashwiniharode0 <115207004+Ashwiniharode0@users.noreply.github.com>
2024-10-05 20:53:23 +0530

    Time: 119 ms (90.91%), Space: 70.5 MB (71.78%) - LeetHub

--- 1694-make-sum-divisible-by-p/1694-make-sum-divisible-by-p.cpp
+++ 1694-make-sum-divisible-by-p/1694-make-sum-divisible-by-p.cpp
@@ -1,36 +1,36 @@
-#include <vector>
-#include <unordered_map>
-using namespace std;
-
 class Solution {
 public:
     int minSubarray(vector<int>& nums, int p) {
-        long totalSum = 0;
-        for (int num : nums) {
-            totalSum += num;
+        int n=nums.size();
+        int sum = 0;
+        for(int &num : nums){
+            sum = (sum + num) %p;
         }
 
-        // Find the remainder when total sum is divided by p
-        int rem = totalSum % p;
-        if (rem == 0) return 0; // If the remainder is 0, no subarray needs to be removed
+        int target = sum % p;
 
-        unordered_map<int, int> prefixMod;
-        prefixMod[0] = -1;  // Initialize for handling full prefix
-        long prefixSum = 0;
-        int minLength = nums.size();
+        if(target == 0){
+            return 0;
+        }
 
-        for (int i = 0; i < nums.size(); ++i) {
-            prefixSum += nums[i];
-            int currentMod = prefixSum % p;
-            int targetMod = (currentMod - rem + p) % p;
+        unordered_map<int,int> mp;
 
-            if (prefixMod.find(targetMod) != prefixMod.end()) {
-                minLength = min(minLength, i - prefixMod[targetMod]);
+        int curr = 0;
+        mp[0] = -1;
+
+        int result=n;
+
+        for(int j=0;j<n;j++){
+            curr = (curr + nums[j]) % p;
+
+            int remain = (curr - target + p) % p;
+            if(mp.find(remain) != mp.end()){
+                result = min(result,j-mp[remain]);
             }
 
-            prefixMod[currentMod] = i;
+            mp[curr] = j;
         }
 
-        return minLength == nums.size() ? -1 : minLength;
+        return result == n ?-1:result;
     }
 };
